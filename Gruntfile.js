414b91393f44d98edbfc52ad888131de7fbcc764
Thiago Gomes <e.thiago.06@gmail.com>
2018-02-23 15:02:27 +0000

    Corrige package e grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,74 +2,81 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     clean: {
         dist: {
-            src: ['dist'],
+            src: ["dist"],
             force: true
         }
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: "/*! <%= pkg.name %> <%= grunt.template.today('yyyy-mm-dd') %> */\n"
       },
       app: {
-        src: ['app/**/*.js', '!app/bower_components/**', '!app/**/*_test.js'] ,
-        dest: 'dist/js/app.min.js',
+        src: ["app/**/*.js", "!app/**/*_test.js"] ,
+        dest: "dist/js/app.min.js",
       },
-      vendor: {
-          dest: 'dist/js/vendor.min.js',
-          src: [
-            'app/bower_components/angular/angular.js',
-            'app/bower_components/angular-route/angular-route.js',
-        ],
-      }
+    },
+    concat: {
+        options: {
+            separator: ";",
+        },
+        jsvendor: {
+            dest: "dist/js/vendor.min.js",
+            src: [
+                "node_modules/angular/angular.min.js",
+                "node_modules/angular-route/angular-route.min.js",
+            ],
+
+        },
     },
     cssmin: {
         options: {
             mergeIntoShorthands: false,
             roundingPrecision: -1,
             flatten: true,
-            filter: 'isFile',
+            filter: "isFile",
         },
         target: {
             files: {
-                'dist/css/app.min.css': ['app/**/*.css', '!app/bower_components/**'],
-                'dist/css/vendor.min.css': [
-                    'app/bower_components/html5-boilerplate/dist/css/normalize.css',
-                    'app/bower_components/html5-boilerplate/dist/css/main.css',
+                "dist/css/app.min.css": ["app/**/*.css"],
+                "dist/css/vendor.min.css": [
+                    "node_modules/html5-boilerplate/dist/css/normalize.css",
+                    "node_modules/html5-boilerplate/dist/css/main.css",
                 ],
             }
         }
     },
     copy: {
       modernizrjs: {
-          src: ['app/bower_components/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js'],
-          dest: 'dist/js/modernizr.min.js',
+          src: ["node_modules/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js"],
+          dest: "dist/js/modernizr.min.js",
       },
       index: {
         expand: true,
-        src: 'app/index*.html',
-        dest: 'dist/',
+        src: "app/index*.html",
+        dest: "dist/",
         flatten: true,
-        filter: 'isFile',
+        filter: "isFile",
       },
       views:{
         expand: true,
-        src: ['app/**/*.html', '!app/bower_components/**', '!app/index*.html'],
-        dest: 'dist/views/',
+        src: ["app/**/*.html", "!app/index*.html"],
+        dest: "dist/views/",
         flatten: true,
-        filter: 'isFile',
+        filter: "isFile",
       }
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-concat");
 
   // Default task(s).
-  grunt.registerTask('default', ['clean', 'uglify', 'cssmin', 'copy']);
+  grunt.registerTask("default", ["clean", "uglify", "cssmin", "copy", "concat"]);
 
 };
