1f23510bed895a28e0cbfea25f5b0d2bcf22d0f2
ethiago <e.thiago.06@gmail.com>
2018-02-23 13:38:00 -0300

    Corrige karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,11 @@ module.exports = function(config) {
     basePath: './app',
 
     files: [
-      '../dist/**/*.js',
+      '../node_modules/angular/angular.js',
+      '../node_modules/angular-route/angular-route.js',
+      '../node_modules/angular-mocks/angular-mocks.js',
+      'components/**/*.js',   
+      'view*/**/*.js'
     ],
 
     autoWatch: true,
