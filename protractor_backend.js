b72bbb51bf5ed6cde126ec33ecb4659c9cd049fc
ethiago <e.thiago.06@gmail.com>
2018-02-25 15:06:18 -0300

    Habilita cors no backend mockado

--- protractor_backend.js
+++ protractor_backend.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 var mockResponseCollection = { id: 1, name: 'collection', discs:[ { id: 1, name: 'D1'}, { id:2, name: 'D2'} ] };
+
+app.use(cors())
 
 app.get('/meta', (req, res) => res.send( { name: "Disc0!", version:"0.0.0"  } ));
 app.get('/collection/*', (req, res) => res.send( mockResponseCollection ));
