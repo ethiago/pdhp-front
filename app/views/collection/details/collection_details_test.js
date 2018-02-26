d5c4cc54c4ff88c54b2e4aa8e0b586cfa57fc04f
ethiago <e.thiago.06@gmail.com>
2018-02-26 20:37:43 -0300

    Corrige test que utilizava resource

--- app/views/collection/details/collection_details_test.js
+++ app/views/collection/details/collection_details_test.js
@@ -7,13 +7,11 @@ describe('pdhp.collection.details', function() {
       $scope,
       mockapiFactory,
       mockParam,
-      queryDeferred,
       mockResponse = { id: 1, name: 'collection', discs:[ { id: 1, name: 'D1'}, { id:2, name: 'D2'} ] };
 
   beforeEach(module('pdhp.collection.details'));
 
-  beforeEach(inject(function(_$q_, _$rootScope_) {
-    $q = _$q_;
+  beforeEach(inject(function(__$rootScope_) {
     $rootScope = _$rootScope_;
   }));
 
@@ -23,9 +21,8 @@ describe('pdhp.collection.details', function() {
     mockParam = { id: 0 };
 
     mockapiFactory = {
-      getCollection: function(id) {
-        queryDeferred = $q.defer();
-        return { $promise: queryDeferred.promise };
+      getCollection: function(id) { 
+        return mockResponse;
       }
     }
     
@@ -48,6 +45,7 @@ describe('pdhp.collection.details', function() {
     it('should define value for collection', function() {
       
       expect($scope.collection).toEqual(mockResponse);
+      
       
     });
 
